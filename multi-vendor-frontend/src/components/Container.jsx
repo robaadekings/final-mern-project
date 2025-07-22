bd1012da46542ef584d9d71d5bbcb6473bf9a192
robaadekings01@gmail.com <robaadekings01@gmail.com>
2025-07-22 23:19:43 +0300

    "mombile responsive"

--- multi-vendor-frontend/src/components/Container.jsx
+++ multi-vendor-frontend/src/components/Container.jsx
@@ -1,6 +1,6 @@
 const Container = ({ children }) => {
     return (
-        <div className="max-w-7xl mx-auto px-4 py-6">
+        <div className="max-w-7xl mx-auto px-2 sm:px-4 py-4 sm:py-6">
             {children}
         </div>
     );
